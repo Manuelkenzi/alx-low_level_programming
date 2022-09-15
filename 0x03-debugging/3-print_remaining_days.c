a9eaced9640eb98a8616c71c2af660b8caedc59f
Emmanuel Okechukwu <50584601+Manuelkenzi@users.noreply.github.com>
2022-09-15 19:33:03 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,6 @@
+
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -12,7 +13,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -34,4 +35,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
+}
 }
