c0c201db11f0576e0340894964b68e89a7699a5e
Emmanuel Okechukwu <50584601+Manuelkenzi@users.noreply.github.com>
2022-10-14 01:42:59 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,9 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
-
-#include <stdarg.h>
-#include <stdio.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif
-Footer
+#endif /*VARIADIC_FUNCTIONS_H*/
